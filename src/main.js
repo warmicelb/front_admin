14e4b997cb7cad5f64706d4df738497140c49705
bin.liu <bin.liu@trip.com>
2023-01-30 15:45:01 +0800

    feat(init):接入 element ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+/* 引入element组件 */
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
